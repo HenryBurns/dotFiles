9b2b5b64e8bd4b326e42f162ec80f6077d9b305d
Henry <hhburnsboy@gmail.com>
2025-05-07 11:33:19 -0700

    Create setup.sh
    Add setup dotfile to install zsh vim and git for me.

---
+++ setup.sh
@@ -0,0 +1,23 @@
+# Move over general dotfile configs.
+
+#######
+# GIT #
+#######
+cp .gitignore_global ~/
+
+#######
+# ZSH #
+#######
+# Install Oh-my-zsh. This will move any .zshrc file to .zshrc.pre-oh-my-zsh.
+sudo apt-get install zsh
+chsh -s $(which zsh)
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# cp -t ~/ .zshrc .zshrc.pre-oh-my-zsh 
+
+
+#######
+# VIM #
+#######
+cp .vimrc ~/
+mkdir -p ~/.vim/colors/
+wget https://raw.githubusercontent.com/sjl/badwolf/refs/heads/master/colors/badwolf.vim -O ~/.vim/colors/badwolf.vim
